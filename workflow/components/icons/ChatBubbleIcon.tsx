3692e898ecb7f3ac1248a7d8aa6d917a60de5d26
Revo <yazidalami1998@gmail.com>
2025-11-07 17:12:53 +0100

    Delete workflow directory

--- workflow/components/icons/ChatBubbleIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const ChatBubbleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193l-3.722.28c-.443.034-.864.196-1.23.45l-4.136 2.955a.75.75 0 01-1.037-.621v-4.329c0-.425-.216-.814-.582-1.047l-4.502-2.923a2.25 2.25 0 01-1.156-2.015v-2.223c0-1.242 1.008-2.25 2.25-2.25h13.5a2.25 2.25 0 012.25 2.25v.741" />
-    </svg>
-);
